1b9371aecdc8c98b930c7555d126692888479fd0
ozcan <ozcan@OZCAN>
2024-01-04 23:37:00 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri eklendi : " + customer.FirstName + " " + customer.LastName);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri silindi : " + customer.FirstName + " " + customer.LastName);
+
+        }
+        public void GetList(Customer[] customers)
+        {
+            Console.WriteLine("Mevcut müşteriler :");
+            foreach (Customer customer in customers) 
+            {
+                Console.WriteLine(customer.FirstName + " " + customer.LastName);
+            }
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassMetotDemo;
+Customer customer1 = new Customer()
+{
+    Id = 1,
+    FirstName = "Özcan",
+    LastName = "Yalçın"
+};
+
+Customer customer2 = new Customer()
+{
+    Id = 2,
+    FirstName = "Ercan",
+    LastName = "Demir"
+};
+Customer[]customers=new Customer[] {customer1, customer2};  
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(customer1);
+customerManager.Add(customer2);
+Console.WriteLine();
+customerManager.Delete(customer1);
+customerManager.Delete(customer2);
+Console.WriteLine();
+customerManager.GetList(customers);
